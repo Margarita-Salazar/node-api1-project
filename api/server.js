6e449dbaf1ee0beae189807b9fb7b649580cf3bc
unknown <margarita.salazar.mls@gmail.com>
2021-06-01 22:41:26 -0500

    added endpoing to retreive all users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,36 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+const { restart } = require('nodemon')
+
+const server = express()
+
+const User = require('./users/model')
+
+server.use(express.json())
+
+server.get('/api/users', async (req,res)=>{
+    try{
+        const user = await User.find()
+        res.json(user)
+    }catch(err){
+        res.json({
+            message: 'The user information could not be retrieved'
+        })
+    }
+})
+server.get('/api/users/:id', (req,res)=>{
+    
+})
+server.post('/api/users', (req,res)=>{
+    
+})
+server.delete('/api/users', (req,res)=>{
+    
+})
+server.put('/api/users', (req,res)=>{
+    
+})
+
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
